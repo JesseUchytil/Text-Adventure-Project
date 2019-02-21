21cab86734d6058438a2fecdac5027735da8f652
Linkpro4 <46803852+Linkpro4@users.noreply.github.com>
2019-02-20 19:18:26 -0600

    Intro and Outro events
    This is just a rough outline of the intro and outline events. These can be changed as I just wanted to get the main story parts put in there.

--- Text Adventure Project/OutroEvent.cpp
+++ Text Adventure Project/OutroEvent.cpp
@@ -7,4 +7,11 @@ OutroEvent::OutroEvent()
 void OutroEvent::runEvent()
 {
 	std::cout << "Running Outro Event" << std::endl;
+
+	std::cout << "You finish the last puzzle and see sunlight protruding through the window of the backdoor" << std::endl;
+	std::cout << "You begin to hear the voice from the first room of the house" << std::endl;
+	std::cout << "Well done guest, I can't believe anyone was able to beat those puzzles" << std::endl;
+	std::cout << "You are now free to leave the house" << std::endl;
+	std::cout << "You push open the back door to bright sunlight and freedom, but you feel that you are forgetting about something" << std::endl;
+	std::cout << "The End?" << std::endl;
 }
